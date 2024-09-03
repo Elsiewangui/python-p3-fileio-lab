21ceed103cced75c4febe46a14bf300a4ac27b2d
Wangui <elswangui@gmail.com>
2024-09-03 17:45:41 +0300

    file i/o

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,22 @@
 def write_file(file_name, file_content):
-    pass
+         file_name = f"{file_name}.txt"
+         with open(file_name, 'w') as file:
+            file.write(file_content)
 
-def append_file(file_name, append_content):
-    pass
+
+def append_file(file_name, file_content):
+         file_name = f"{file_name}.txt"
+         with open(file_name, 'a') as file:
+            file.write(file_content)
 
 def read_file(file_name):
-    pass
+         file_name = f"{file_name}.txt"
+         with open(file_name, 'r') as file:
+            return file.read()
+
+
+
+write_file(file_name="logfile", file_content="Log 1: 5 bananas added\n")
+append_file(file_name="logfile", file_content="Log 2: 3 bananas subtracted\n")
+content = read_file(file_name="logfile")
+print(content)
